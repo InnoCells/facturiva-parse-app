6f20957a3a7937a8798b8f088815b322d000449e
Ernest Roca <ernest@partners.innocells.io>
2017-11-21 15:47:45 +0100

    add async

--- cloud/triggers/beforeSave.js
+++ cloud/triggers/beforeSave.js
@@ -1,10 +1,9 @@
-Parse.Cloud.beforeSave('Tickets', function(request, response) {
-
+Parse.Cloud.beforeSave('Tickets', async function(request, response) {
   const ticketQuery = new Parse.Query('Tickets');
   ticketQuery.equalTo('objectId', request.object.id);
 
   const result = await ticketQuery.first();
-  
+
   request.log.error(result.id);
 
   response.success();
