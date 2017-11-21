2e60352bdbaf7fb81f4e41d88a3cc8087eb184c4
Ernest Roca <ernest@partners.innocells.io>
2017-11-21 15:52:13 +0100

    add try catch

--- cloud/triggers/beforeSave.js
+++ cloud/triggers/beforeSave.js
@@ -1,10 +1,14 @@
 Parse.Cloud.beforeSave('Tickets', async function(request, response) {
-  const ticketQuery = new Parse.Query('Tickets');
-  ticketQuery.equalTo('objectId', request.object.id);
+  try {
+    const ticketQuery = new Parse.Query('Tickets');
+    ticketQuery.equalTo('objectId', request.object.id);
 
-  const result = await ticketQuery.first();
+    const result = await ticketQuery.first();
 
-  request.log.error(result.id);
+    request.log.error(result.get('numero'));
 
-  response.success();
+    response.success();
+  } catch (error) {
+    response.error('Error: ', error);
+  }
 });
