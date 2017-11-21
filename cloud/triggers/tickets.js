cac9595b5623b5301490d581562f2103aaa8ea20
Ernest Roca <ernest@partners.innocells.io>
2017-11-21 10:36:55 +0100

    Query

--- cloud/triggers/tickets.js
+++ cloud/triggers/tickets.js
@@ -1,3 +1,6 @@
-Parse.Cloud.afterSave('Tickets', function(request) {
+Parse.Cloud.afterSave('Tickets', async function(request) {
   request.log.info('Request: ', request.object.id);
+  const ticketQuery = new Parse.Query('Tickets');
+  ticketQuery.equalTo('objectId', request.object.id);
+  const ticketResult = await ticketQuery.first();
 });
