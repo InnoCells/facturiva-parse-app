15133d798a85ec7a832c9fdc2e1a83bb2c667f5a
Ernest Roca <ernest@partners.innocells.io>
2017-11-21 15:46:18 +0100

    asd

--- cloud/triggers/beforeSave.js
+++ cloud/triggers/beforeSave.js
@@ -1,4 +1,11 @@
 Parse.Cloud.beforeSave('Tickets', function(request, response) {
-  request.log.error(request.object.id);
+
+  const ticketQuery = new Parse.Query('Tickets');
+  ticketQuery.equalTo('objectId', request.object.id);
+
+  const result = await ticketQuery.first();
+  
+  request.log.error(result.id);
+
   response.success();
 });
