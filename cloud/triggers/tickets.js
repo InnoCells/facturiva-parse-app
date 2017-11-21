2536126dff3a810769b03028f02ab722ade44c52
Ernest Roca <ernest@partners.innocells.io>
2017-11-21 15:01:13 +0100

    trigger

--- cloud/triggers/tickets.js
+++ cloud/triggers/tickets.js
@@ -4,13 +4,13 @@ Parse.Cloud.beforeSave('Tickets', async function(request, response) {
   const logger = request.log;
   try {
     const ticketQuery = new Parse.Query('Tickets');
-    ticketQuery.equalTo('objectId', request.object.get('objectId'));
+    ticketQuery.equalTo('objectId', request.object.id);
     // ticketQuery.include('merchant');
     // ticketQuery.include('user');
 
     const ticketResult = await ticketQuery.first();
 
-    logger.error('ticketResult: ', request.object.get('objectId'));
+    logger.error('ticketResult: ', request.object.id);
 
     if (ticketResult) {
       logger.error('Entra en el if');
